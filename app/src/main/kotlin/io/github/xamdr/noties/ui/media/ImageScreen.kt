b66b0f5b767d04b04baae8753b1bdb6447bbdc00
XamDR <maxdr.mat@gmail.com>
2023-10-18 22:36:38 -0500

    We added basic zoom-pan for images.

--- app/src/main/kotlin/io/github/xamdr/noties/ui/media/ImageScreen.kt
+++ app/src/main/kotlin/io/github/xamdr/noties/ui/media/ImageScreen.kt
@@ -16,7 +16,6 @@ import coil.request.ImageRequest
 import io.github.xamdr.noties.R
 import io.github.xamdr.noties.domain.model.MediaItem
 import io.github.xamdr.noties.ui.helpers.DevicePreviews
-import io.github.xamdr.noties.ui.helpers.clickableWithoutRipple
 import io.github.xamdr.noties.ui.theme.NotiesTheme
 
 @Composable
@@ -24,15 +23,22 @@ fun ImageScreen(
 	item: MediaItem,
 	onClick: () -> Unit
 ) {
-	Box(modifier = Modifier.clickableWithoutRipple(onClick = onClick)) {
+	val zoomState = rememberZoomState(maxScale = 2f)
+	Box {
 		AsyncImage(
 			model = ImageRequest.Builder(LocalContext.current)
 				.data(item.uri)
 				.build(),
 			contentDescription = stringResource(id = R.string.user_generated_image),
-			modifier = Modifier.fillMaxSize(),
 			alignment = Alignment.Center,
-			contentScale = ContentScale.Fit
+			contentScale = ContentScale.Fit,
+			modifier = Modifier
+				.fillMaxSize()
+				.zoomable(
+					zoomState = zoomState,
+					onTap = onClick,
+					onDoubleTap = { offset -> zoomState.toggleScale(2f, offset) }
+				)
 		)
 	}
 }
